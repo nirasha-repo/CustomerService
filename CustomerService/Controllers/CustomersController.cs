82eb83550100cc432b624638fc8cee60ec563766
NIRASHA <NIRASHA@DESKTOP-O6NPSSU>
2019-11-29 18:56:32 +1100

    implement api endpoints

--- CustomerService/Controllers/CustomersController.cs
+++ CustomerService/Controllers/CustomersController.cs
@@ -9,7 +9,7 @@ using CustomerService.Models;
 
 namespace SampleService.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/customers")]
     [ApiController]
     public class CustomersController : ControllerBase
     {
@@ -21,6 +21,7 @@ namespace SampleService.Controllers
         }
 
         // GET: api/customers
+        // ex: https://localhost:5001/api/customers
         [HttpGet]
         public IEnumerable<Customer> GetCustomers()
         {
@@ -28,45 +29,68 @@ namespace SampleService.Controllers
         }
 
         // GET: api/customers/5
-        //[HttpGet("{id}")]
-        //public async Task<IActionResult> GetCustomer([FromRoute] int id)
-        //{
-        //    if (!ModelState.IsValid)
-        //    {
-        //        return BadRequest(ModelState);
-        //    }
+        // ex: https://localhost:5001/api/customers/4
+        [HttpGet]
+        [Route("{id}")]        
+        public async Task<IActionResult> GetCustomer([FromRoute] int id)
+        {
+            Customer customer;
 
-        //    var customer = await _context.Customers.FindAsync(id);
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-        //    if (customer == null)
-        //    {
-        //        return NotFound();
-        //    }
+                customer = await _context.Customers.FindAsync(id);
 
-        //    return Ok(customer);
-        //}
+                if (customer == null)
+                {
+                    return NotFound();
+                }
+            }
+            catch (Exception ex)
+            {
+                // log exception details
+                return StatusCode(500);
+            }
+            
+            return Ok(customer);
+        }
 
         // GET: api/customers
+        // ex: https://localhost:5001/api/customers/getCustomerBySearchName/nir
         [HttpGet]
-        [Route("{searchString}")]
-        public IActionResult GetCustomer(string searchString)
+        [Route("getCustomerBySearchName/{searchString}")]
+        public IActionResult GetCustomerBySearchName(string searchString)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            Customer customer;
 
-            if (string.IsNullOrEmpty(searchString))
+            try
             {
-                return BadRequest("Search String Is Empty!");
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
+
+                if (string.IsNullOrEmpty(searchString))
+                {
+                    return BadRequest("Search String Is Empty!");
+                }
+
+                customer = FindCustomer(searchString);
+
+                if (customer == null)
+                {
+                    return NotFound("Customer Not Found!");
+                }
             }
-
-            var customer = FindCustomer(searchString);
-
-            if (customer == null)
+            catch (Exception ex)
             {
-                return NotFound("Customer Not Found!");
-            }
+                // log exception details
+                return StatusCode(500);
+            }            
 
             return Ok(customer);
         }
@@ -75,21 +99,29 @@ namespace SampleService.Controllers
         // POST: api/customers
         [HttpPost]
         public async Task<IActionResult> PostCustomer([FromBody] Customer customer)
-        {
-            if (!ModelState.IsValid)
+        {          
+            try
             {
-                return BadRequest(ModelState);
-            }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            if(CustomerExists(customer.Id))
+                if (CustomerExists(customer.Id))
+                {
+                    return BadRequest("Customer Already Exists!");
+                }
+
+                _context.Customers.Add(customer);
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+            }
+            catch (Exception ex)
             {
-                return BadRequest("Customer Already Exists!");
-            }
-
-            _context.Customers.Add(customer);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+                // log exception details
+                return StatusCode(500);
+            }            
         }
 
 
@@ -97,44 +129,63 @@ namespace SampleService.Controllers
         [HttpPut("{id}")]
         public async Task<IActionResult> PutCustomer([FromRoute] int id, [FromBody] Customer customer)
         {
-            if (!ModelState.IsValid)
+            try
             {
-                return BadRequest(ModelState);
-            }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            if (id != customer.Id)
+                if (id != customer.Id)
+                {
+                    return BadRequest("Incorrect Customer Ids!");
+                }
+
+                if (!CustomerExists(id))
+                {
+                    return NotFound("Customer Not Found!");
+                }
+
+                _context.Entry(customer).State = EntityState.Modified;
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+            }
+            catch (Exception ex)
             {
-                return BadRequest("Incorrect Customer Ids!");
-            }
-
-            if (!CustomerExists(id))
-            {
-                return NotFound("Customer Not Found!");
-            }
-
-            _context.Entry(customer).State = EntityState.Modified;
-            await _context.SaveChangesAsync();                       
-
-            return CreatedAtAction("GetCustomer", new { id = customer.Id }, customer);
+                // log exception details
+                return StatusCode(500);
+            }            
         }
 
         // DELETE: api/customers/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeleteCustomer([FromRoute] int id)
         {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+            Customer customer;
 
-            var customer = await _context.Customers.FindAsync(id);
-            if (customer == null)
+            try
             {
-                return NotFound("Customer Not Found!");
-            }
+                if (!ModelState.IsValid)
+                {
+                    return BadRequest(ModelState);
+                }
 
-            _context.Customers.Remove(customer);
-            await _context.SaveChangesAsync();
+                customer = await _context.Customers.FindAsync(id);
+                
+                if (customer == null)
+                {
+                    return NotFound("Customer Not Found!");
+                }
+
+                _context.Customers.Remove(customer);
+                await _context.SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                // log exception details
+                return StatusCode(500);
+            }            
 
             return Ok(customer);
         }
@@ -146,14 +197,14 @@ namespace SampleService.Controllers
 
         private Customer FindCustomer(string searchString)
         { 
-            Customer customer = _context.Customers.First(c => c.FirstName.StartsWith(searchString, StringComparison.CurrentCultureIgnoreCase));
+            Customer customer = _context.Customers.FirstOrDefault(c => c.FirstName.StartsWith(searchString, StringComparison.CurrentCultureIgnoreCase));
 
             if(customer != null)
             {
                 return customer;
             }
 
-            customer = _context.Customers.First(c => c.LastName.StartsWith(searchString, StringComparison.CurrentCultureIgnoreCase));
+            customer = _context.Customers.FirstOrDefault(c => c.LastName.StartsWith(searchString, StringComparison.CurrentCultureIgnoreCase));
 
             return customer;
         }
